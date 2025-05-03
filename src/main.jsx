6fac093ad189b151091c336f78533dc57015a183
Meherin-Islam <meherinislam59@gmail.com>
2025-05-03 13:08:28 +0600

    roots added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,56 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './components/Root/Root';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+import Home from './components/Home/Home';
+import Dashboard from './components/Dashboard/Dashboard';
+import Statistics from './components/Statistics/Statistics';
+import GadgetDetail from './components/GadgetDetail/GadgetDetail';
+import ContactUs from './components/Contact/ContactUs';
+import {HelmetProvider} from 'react-helmet-async';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: 'dashboard',
+        element: <Dashboard></Dashboard>,
+        loader: () => fetch('/gadgetsData.json')
+      },
+      {
+        path: 'statistics',
+        element: <Statistics></Statistics>
+      },
+      {
+        path: 'gadgets/:product_id',
+        element: <GadgetDetail></GadgetDetail>,
+        loader: () => fetch('/gadgetsData.json')
+      },
+      {
+        path: 'contactUs',
+        element: <ContactUs></ContactUs>
+      }
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HelmetProvider>
+    <RouterProvider router={router} />
+    </HelmetProvider>
   </StrictMode>,
 )
